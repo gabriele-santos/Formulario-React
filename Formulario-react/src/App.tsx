0ac62f97fb30269b1afe994fa86703110289bc65
gabriele santos <gabbisn5@gmail.com>
2023-11-06 19:58:15 -0300

    Arquivo principal renderizando cada componente e utilizando estados

--- Formulario-react/src/App.tsx
+++ Formulario-react/src/App.tsx
@@ -2,40 +2,85 @@ import { useState } from 'react'
 import PersonalForm from './components/PersonalForm ';
 import AddressForm from './components/AddressForm';
 import ProfessionalForm from './components/ProfessionalForm';
+import DataSubmit from './components/DataSubmit';
 import './App.css'
-import { FormStateType } from './types';
-/* import { FormStateType } from './types'; */
 
 function App() {
   const INICIAL_STATE = {
     name: '',
     email: '',
-    cpf: 0,
+    cpf: '',
     address: '',
     city: '',
-    state: '', //DÚVIDA
-    residence: 'Casa',
+    state: '',
+    residence: '',
     curriculum: '',
     office: '',
-    jobDescription: '',
-  } as FormStateType
+    roleDescription: '',
+  }
 
   const [formValues, setFormValues] = useState(INICIAL_STATE);
+  const [mouseEnter, setMouseEnter] = useState
+    (false);
+  const [submitData, setSubmitData] = useState({});
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) {
-    const { name, value } = event.target;
+    let { name, value } = event.target;
+    if (name === 'name') {
+      value = value.toUpperCase();
+    }
+    if (name === 'address') {
+      value = value.replace(/[^\w\s]/g, '');
+
+    }
     setFormValues({
       ...formValues,
       [name]: value,
     })
+    console.log('setformValues', setFormValues);
+
+  }// Função genérica para atualizar os valores dos inputs
+
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const verifyNumber = /^\d/.test(value);
+    if (name === 'city' && verifyNumber) {
+
+      setFormValues((prevState) => ({
+        ...prevState,
+        [name]: '',
+      }));
+    }
+  }
+
+  function handleMouseEnter() {
+    if (!mouseEnter) {
+      setMouseEnter(true);
+      alert('Preencha com cuidado esta informação!') //Para o alert aparecer apenas uma vez
+    }
+  }
+
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setSubmitData(formValues);
+  }
+
+  function resetForm() {
+    setFormValues(INICIAL_STATE);
   }
 
   return (
-    <form >
-      <PersonalForm handleChange={handleChange} formValues={formValues} />
-      <AddressForm />
-      <ProfessionalForm formValues={formValues} />
-    </form>
+    <form onSubmit={handleSubmit}>
+      <PersonalForm onChange={handleChange} formValues={formValues} />
+      <AddressForm onChange={handleChange} formValues={formValues} onBlur={handleBlur} />
+      <ProfessionalForm onChange={handleChange} formValues={formValues} onMouseEnter={handleMouseEnter} mouseEnter={mouseEnter} />
+      <button type="submit" >Enviar</button>{
+        submitData && <DataSubmit formValues={formValues} />
+      }
+      <button type="reset" onClick={() => resetForm()}>Limpar</button>
+    </form >
   )
 }
 
